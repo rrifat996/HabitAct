744aa146a7765ef1bfa71e2fff2ac6a6b7918021
Rıfat Altaş <rifataltasrmrdnsrgntlzr996@gmail.com>
2022-07-07 12:15:53 +0300

    login referance and simple buttons added

--- app/src/main/java/com/example/csprojedeneme/MainActivity.java
+++ app/src/main/java/com/example/csprojedeneme/MainActivity.java
@@ -3,12 +3,55 @@ package com.example.csprojedeneme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextUsername;
+    private EditText editTextPassword;
+
+    private EditText ediTextUsername2;
+    private EditText editTextPassword2;
+    private EditText editTextPassword3;
+
+    private Button btnLogin;
+    private Button btnSignup;
+    private Button btnSignup2;
+    
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextUsername = findViewById(R.id.editTextUsername);
+        editTextPassword = findViewById(R.id.editTextPassword);
+
+        editTextUsername = findViewById(R.id.editTextUsername2);
+        editTextPassword = findViewById(R.id.editTextPassword2);
+        editTextPassword = findViewById(R.id.editTextPassword3);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnSignup2 = findViewById(R.id.btnSignup2);
+
+        btnLogin.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                return false;
+            }
+        });
+
+    }
+    public void createUser(){
+
+    }
+    public void loginUser(){
+
     }
 }
