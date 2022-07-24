ff2e536a18a09ec9ee0de1e600c8822899be953c
rrifat <rifataltasrmrdnsrgntlzr996@gmail.com>
2022-07-25 01:11:33 +0300

    updated challenge selection

--- app/src/main/java/com/example/csprojedeneme/ChallengeAdapter.java
+++ app/src/main/java/com/example/csprojedeneme/ChallengeAdapter.java
@@ -8,12 +8,14 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 
 public class ChallengeAdapter extends RecyclerView.Adapter {
     private ArrayList<Challenge> mChallenges;
+    private int singleItemSelection = -1;
     private OnItemClickListener mListener;
 
 
@@ -25,21 +27,23 @@ public class ChallengeAdapter extends RecyclerView.Adapter {
         mListener = listener;
     }
 
-    public static class ExampleViewHolder extends RecyclerView.ViewHolder{
+    public class ExampleViewHolder extends RecyclerView.ViewHolder{
 
         public TextView mTitle;
         public TextView mDescription;
         public TextView mCreatorName;
         public ImageView mIcon;
+        public ConstraintLayout changeColor;
 
 
-        public ExampleViewHolder(View itemView, OnItemClickListener listener){
+        public ExampleViewHolder(View itemView, OnItemClickListener listener) {
             super(itemView);
 
             mTitle = itemView.findViewById(R.id.mTitle);
             mDescription = itemView.findViewById(R.id.mDescription);
             mCreatorName = itemView.findViewById(R.id.mCreatorName);
             mIcon = itemView.findViewById(R.id.mImage);
+            changeColor= itemView.findViewById(R.id.changeColor);
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -48,12 +52,21 @@ public class ChallengeAdapter extends RecyclerView.Adapter {
                         int positon = getAbsoluteAdapterPosition();
                         if(positon != RecyclerView.NO_POSITION){
                             listener.onItemClick(positon);
+                            setSingleSelection(positon);
+
                         }
                     }
                 }
             });
         }
-
+    }
+    private void setSingleSelection(int adapterPosition){
+        if(adapterPosition == RecyclerView.NO_POSITION){
+            return;
+        }
+        notifyItemChanged(singleItemSelection);
+        singleItemSelection = adapterPosition;
+        notifyItemChanged(singleItemSelection);
     }
     public ChallengeAdapter(ArrayList<Challenge> challenges){
         mChallenges = challenges;
@@ -69,6 +82,12 @@ public class ChallengeAdapter extends RecyclerView.Adapter {
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(singleItemSelection == position){
+            holder.itemView.findViewById(R.id.changeColor).setBackgroundResource(R.drawable.image_view_border);
+        }else{
+            holder.itemView.findViewById(R.id.changeColor).setBackgroundResource(R.drawable.image_view_border_zero);
+        }
+
         Challenge currentChallenge = mChallenges.get(position);
 
         ExampleViewHolder holder1 = (ExampleViewHolder)holder;
@@ -77,6 +96,7 @@ public class ChallengeAdapter extends RecyclerView.Adapter {
         holder1.mDescription.setText(currentChallenge.getDescription());
         holder1.mTitle.setText(currentChallenge.getChallengeName());
         holder1.mIcon.setImageResource(currentChallenge.getmImageResource());
+        //holder1.changeColor.setBackgroundResource(R.drawable.image_view_border);
 
     }
 
